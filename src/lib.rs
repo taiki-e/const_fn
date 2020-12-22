6612a21e64e4b71a8d063a4e744cef5d98a1d3a5
Taiki Endo <te316e89@gmail.com>
2020-12-23 01:26:58 +0900

    Update allowed lints

--- src/lib.rs
+++ src/lib.rs
@@ -47,8 +47,6 @@
 #![forbid(unsafe_code)]
 #![warn(future_incompatible, rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take requires Rust 1.40
-#![allow(clippy::mem_replace_with_default)]
 
 // older compilers require explicit `extern crate`.
 #[allow(unused_extern_crates)]
