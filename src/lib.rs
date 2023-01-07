4ff0aa3b52c1243524301ebd6d2b6e222fd7b3ed
Taiki Endo <te316e89@gmail.com>
2023-01-08 01:03:00 +0900

    Change crate-level doc style

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,60 @@
-//! An attribute for easy generation of const functions with conditional
-//! compilations.
-//!
-//! # Examples
-//!
-//! ```rust
-//! use const_fn::const_fn;
-//!
-//! // function is `const` on specified version and later compiler (including beta, nightly, and dev build)
-//! #[const_fn("1.36")]
-//! pub const fn version() {
-//!     /* ... */
-//! }
-//!
-//! // function is `const` on nightly compiler (including dev build)
-//! #[const_fn(nightly)]
-//! pub const fn nightly() {
-//!     /* ... */
-//! }
-//!
-//! // function is `const` if `cfg(...)` is true
-//! # #[cfg(any())]
-//! #[const_fn(cfg(...))]
-//! # pub fn _cfg() { unimplemented!() }
-//! pub const fn cfg() {
-//!     /* ... */
-//! }
-//!
-//! // function is `const` if `cfg(feature = "...")` is true
-//! #[const_fn(feature = "...")]
-//! pub const fn feature() {
-//!     /* ... */
-//! }
-//! ```
-//!
-//! ## Use this crate as an optional dependency
-//!
-//! If no arguments are passed, `const_fn` will always make the function `const`.
-//!
-//! Therefore, you can use `const_fn` as an optional dependency by combination with `cfg_attr`.
-//!
-//! ```rust
-//! // function is `const` if `cfg(feature = "...")` is true
-//! #[cfg_attr(feature = "...", const_fn::const_fn)]
-//! pub fn optional() {
-//!     /* ... */
-//! }
-//! ```
-//!
-//! # Alternatives
-//!
-//! This crate is proc-macro, but is very lightweight, and has no dependencies.
-//!
-//! You can manually define declarative macros with similar functionality (see [`if_rust_version`](https://github.com/ogoffart/if_rust_version#examples)), or [you can define the same function twice with different cfg](https://github.com/crossbeam-rs/crossbeam/blob/0b6ea5f69fde8768c1cfac0d3601e0b4325d7997/crossbeam-epoch/src/atomic.rs#L340-L372).
-//! (Note: the former approach requires more macros to be defined depending on the number of version requirements, the latter approach requires more functions to be maintained manually)
+/*!
+An attribute for easy generation of const functions with conditional
+compilations.
+
+# Examples
+
+```rust
+use const_fn::const_fn;
+
+// function is `const` on specified version and later compiler (including beta, nightly, and dev build)
+#[const_fn("1.36")]
+pub const fn version() {
+    /* ... */
+}
+
+// function is `const` on nightly compiler (including dev build)
+#[const_fn(nightly)]
+pub const fn nightly() {
+    /* ... */
+}
+
+// function is `const` if `cfg(...)` is true
+# #[cfg(any())]
+#[const_fn(cfg(...))]
+# pub fn _cfg() { unimplemented!() }
+pub const fn cfg() {
+    /* ... */
+}
+
+// function is `const` if `cfg(feature = "...")` is true
+#[const_fn(feature = "...")]
+pub const fn feature() {
+    /* ... */
+}
+```
+
+## Use this crate as an optional dependency
+
+If no arguments are passed, `const_fn` will always make the function `const`.
+
+Therefore, you can use `const_fn` as an optional dependency by combination with `cfg_attr`.
+
+```rust
+// function is `const` if `cfg(feature = "...")` is true
+#[cfg_attr(feature = "...", const_fn::const_fn)]
+pub fn optional() {
+    /* ... */
+}
+```
+
+# Alternatives
+
+This crate is proc-macro, but is very lightweight, and has no dependencies.
+
+You can manually define declarative macros with similar functionality (see [`if_rust_version`](https://github.com/ogoffart/if_rust_version#examples)), or [you can define the same function twice with different cfg](https://github.com/crossbeam-rs/crossbeam/blob/0b6ea5f69fde8768c1cfac0d3601e0b4325d7997/crossbeam-epoch/src/atomic.rs#L340-L372).
+(Note: the former approach requires more macros to be defined depending on the number of version requirements, the latter approach requires more functions to be maintained manually)
+*/
 
 #![doc(test(
     no_crate_inject,
